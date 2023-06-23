5b89b5276f8ca90df6a75a8fdad4877d29e5a5e4
Partha Sarathi Muduli <66170183+partha7978@users.noreply.github.com>
2023-06-23 23:16:02 +0530

    Update index.js

---
+++ blurry_loading/index.js
@@ -0,0 +1,21 @@
+const loadingText = document.querySelector('.loading-text')
+const background = document.querySelector('.background')
+
+let loading = 0;
+let setBlur = setInterval(load, 20);
+
+function load() {
+    loading ++;
+    
+    if(loading > 99) {
+        clearInterval(setBlur);
+    }
+
+    loadingText.innerText = `${loading}%`
+    loadingText.style.opacity = mapNumber(loading, 0, 100, 1, 0);
+    background.style.filter = `blur(${mapNumber(loading, 0, 100, 30, 0)}px)`
+}
+
+function mapNumber (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
